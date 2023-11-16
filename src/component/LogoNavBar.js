939314e6c4e477fd3fe71b8c905ac1c75b1c066a
freelancing with programming <iosattbsoft2021@gmail.com>
2023-11-16 10:06:32 +0500

    vv

--- src/component/LogoNavBar.js
+++ src/component/LogoNavBar.js
@@ -1,7 +1,7 @@
 function LogoNavBar(){
 
     return <div>
-        <img src = "./logo.gif" width="100%" height={150}/>
+        <img src = "./logo.gif" width="100%" height={150} alt="redamdial section"/>
     </div>
 }
 export default LogoNavBar;
