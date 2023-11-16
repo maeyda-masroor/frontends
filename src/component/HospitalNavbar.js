58c04ffc6384a4559d88fa74438086343c2010c7
freelancing with programming <iosattbsoft2021@gmail.com>
2023-11-16 10:32:01 +0500

    vv

--- src/component/HospitalNavbar.js
+++ src/component/HospitalNavbar.js
@@ -2,8 +2,6 @@ import TopNavBarHospital from './TopNavbarHospital';
 import Navbar from './Navbar';
 import NavbarItem from './NavBarItem';
 import LogoNavBar from './LogoNavBar';
-import SectionIntro from './SectionIntro';
-import FooterHospital from './FooterHospital';
 function NavigationBar(){
     return (
     <div>
